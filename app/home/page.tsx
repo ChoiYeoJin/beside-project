e4bb80c31f03c9e24c7ad1c68b3bddd50fe86160
ChoiYeoJin <sonata7531@gmail.com>
2024-03-16 22:33:28 +0900

    home page 추가

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+import Container from "../components/Container";
+import HomeSearchInput from "../components/HomeSearchInput";
+
+export default function Home() {
+  return (
+    <Container>
+      <HomeSearchInput />
+    </Container>
+  );
+}
