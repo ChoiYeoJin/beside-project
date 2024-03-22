02ced70c7331619ba4110a8d7066e6a88c106c58
ChoiYeoJin <sonata7531@gmail.com>
2024-03-22 12:21:47 +0900

    fetch수정

--- utils/fetch.ts
+++ utils/fetch.ts
@@ -19,32 +19,32 @@ export const fetchData = async <T>(url: string): Promise<T> => {
 };
 
 export const fetchDataAuthorized = async <T>(url: string): Promise<T> => {
-  let errorCount = 0;
-  while (errorCount < 3) {
-    try {
-      const res = await axios.get(SERVER_URL + url, {
+  try {
+    let res = await axios.get(SERVER_URL + url, {
+      headers: {
+        Authorization: `Bearer ${getAccessToken()}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (res.status === 401) {
+      const refreshRes = await fetchPostData<RefreshResponse>(
+        "/api/token/refresh/"
+      );
+      setAccessToken(refreshRes.access);
+
+      res = await axios.get(SERVER_URL + url, {
         headers: {
           Authorization: `Bearer ${getAccessToken()}`,
           "Content-Type": "application/json",
         },
       });
-
-      if (res.status === 401) {
-        const refreshRes = await fetchPostData<RefreshResponse>(
-          "/api/token/refresh/"
-        );
-        setAccessToken(refreshRes.access);
-        errorCount++;
-        continue;
-      }
-
-      return res.data as T;
-    } catch (error: any) {
-      console.error("Unable to fetch data : " + error);
     }
-  }
 
-  throw new Error("Unable to fetch data : " + errorCount);
+    return res.data as T;
+  } catch (error: any) {
+    console.error("Unable to fetch data : " + error);
+  }
 };
 
 const fetchPostData = async <T>(url: string): Promise<T> => {
